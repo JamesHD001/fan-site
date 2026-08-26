9db68481a270f9cead5b682b941c53b66b97606a
JamesHD001 <henry.jameshd@gmail.com>
2026-08-26 21:52:33 +0100

    Add registration OTP verification endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getCurrentUser, updateCurrentUser } = require("../controllers/authController");
+const { register, verifyRegistration, login, getCurrentUser, updateCurrentUser } = require("../controllers/authController");
 const authenticate = require("../middleware/authenticate");
 const validate = require("../middleware/validate");
 const { authLimiter } = require("../middleware/rateLimiters");
@@ -7,6 +7,7 @@ const { registerValidator, loginValidator } = require("../validators/authValidat
 
 const router = express.Router();
 router.post("/register", authLimiter, registerValidator, validate, register);
+router.post("/register/verify", authLimiter, verifyRegistration);
 router.post("/login", authLimiter, loginValidator, validate, login);
 router.get("/me", authenticate, getCurrentUser);
 router.patch("/me", authenticate, updateCurrentUser);
