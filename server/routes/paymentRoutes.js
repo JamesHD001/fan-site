f12f87d9797ac2a79d2fdec5ad3fabfc26fb6f2b
JamesHD001 <henry.jameshd@gmail.com>
2026-08-26 18:18:04 +0100

    Fix malformed payment routes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const authenticate = require("../middleware/authenticate");
 const { verifyPayment } = require("../controllers/paymentController");
-const { authLimiter } = require("../middleware/rateLimiters");
-
-router.post("/register", authLimiter, registerValidator, validate, register);
-router.post("/login", authLimiter, loginValidator, validate, login);
 
 const router = express.Router();
 
