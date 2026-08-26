a465f817d7bd56e265385ceef38a5b8af7f5a114
JamesHD001 <henry.jameshd@gmail.com>
2026-08-26 21:46:18 +0100

    Add profile update route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,41 +1,13 @@
 const express = require("express");
-const {
-  register,
-  login,
-  getCurrentUser,
-} = require("../controllers/authController");
-
+const { register, login, getCurrentUser, updateCurrentUser } = require("../controllers/authController");
 const authenticate = require("../middleware/authenticate");
 const validate = require("../middleware/validate");
 const { authLimiter } = require("../middleware/rateLimiters");
-
-const {
-  registerValidator,
-  loginValidator,
-} = require("../validators/authValidator");
+const { registerValidator, loginValidator } = require("../validators/authValidator");
 
 const router = express.Router();
-
-router.post(
-  "/register",
-  authLimiter,
-  registerValidator,
-  validate,
-  register
-);
-
-router.post(
-  "/login",
-  authLimiter,
-  loginValidator,
-  validate,
-  login
-);
-
-router.get(
-  "/me",
-  authenticate,
-  getCurrentUser
-);
-
+router.post("/register", authLimiter, registerValidator, validate, register);
+router.post("/login", authLimiter, loginValidator, validate, login);
+router.get("/me", authenticate, getCurrentUser);
+router.patch("/me", authenticate, updateCurrentUser);
 module.exports = router;
