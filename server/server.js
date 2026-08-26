64ab031040defe5e259c2496031807f9a60c7db5
JamesHD001 <henry.jameshd@gmail.com>
2026-08-26 22:56:51 +0100

    Start automated membership maintenance after database connection

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ validateEnv();
 
 const app = require("./app");
 const connectDatabase = require("./config/database");
+const { startMembershipMaintenance } = require("./jobs/membershipMaintenance");
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     await connectDatabase();
+    startMembershipMaintenance();
 
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
