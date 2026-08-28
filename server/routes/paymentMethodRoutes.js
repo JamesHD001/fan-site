33b56a7f8c97b3c68392c256b19ddab166762e13
JamesHD001 <henry.jameshd@gmail.com>
2026-08-28 06:29:03 +0100

    Add card token capture route

--- server/routes/paymentMethodRoutes.js
+++ server/routes/paymentMethodRoutes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const authenticate = require("../middleware/authenticate");
-const {
-  listPaymentMethods,
-  setDefaultPaymentMethod,
-  removePaymentMethod,
-} = require("../controllers/paymentMethodController");
+const { listPaymentMethods, setDefaultPaymentMethod, removePaymentMethod } = require("../controllers/paymentMethodController");
+const { saveCardFromPayment } = require("../controllers/paymentMethodTokenController");
 
 const router = express.Router();
 router.use(authenticate);
 router.get("/", listPaymentMethods);
+router.post("/cards/from-payment", saveCardFromPayment);
 router.patch("/:id/default", setDefaultPaymentMethod);
 router.delete("/:id", removePaymentMethod);
 
