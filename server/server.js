eab78fbb0cc2a09e8aab5d9d9a70157e71d1c142
JamesHD001 <henry.jameshd@gmail.com>
2026-09-05 12:38:39 +0100

    Improve production server startup logging

--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ const app=require("./app");
 const connectDatabase=require("./config/database");
 const {startMembershipMaintenance}=require("./jobs/membershipMaintenance");
 const PORT=process.env.PORT||5000;
-const startServer=async()=>{try{await connectDatabase();startMembershipMaintenance();app.listen(PORT,()=>console.log(`Server running on http://localhost:${PORT}`))}catch(error){console.error("Unable to start server.");process.exit(1)}};
+const startServer=async()=>{try{await connectDatabase();startMembershipMaintenance();app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))}catch(error){console.error("Unable to start server.");process.exit(1)}};
 startServer();
